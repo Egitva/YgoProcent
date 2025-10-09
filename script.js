c3116d5ca7c8a3c995f74b21b346b53d0b9ddabe
Egitva <9026479948e@gmail.com>
2025-10-09 12:01:18 +0500

    draw

--- script.js
+++ script.js
@@ -2,11 +2,12 @@ document.getElementById("sumbit").addEventListener("click", function() {
     const userDeck = document.getElementById("player_in").value;
     const userProcent = document.getElementById("cards_need").value;
     const userGarn = document.getElementById("garnet").value;
+    const userDraw = document.getElementById("draw").value;
     document.getElementById('output').innerHTML=null;
-    document.getElementById('output').innerHTML+=`<p>Вероятность надровать нужную карту: ${(1-(factorial(userDeck-userProcent)/(factorial(5)*(factorial((userDeck-userProcent)-5))))/(factorial(userDeck)/(factorial(5)*(factorial((userDeck)-5)))))*100}%</p>`
-    document.getElementById('output').innerHTML+=`<p>Вероятность надровать гарнет: ${(1-(factorial(userDeck-userGarn)/(factorial(5)*(factorial((userDeck-userGarn)-5))))/(factorial(userDeck)/(factorial(5)*(factorial((userDeck)-5)))))*100}%</p>`
+    document.getElementById('output').innerHTML+=`<p>Вероятность надровать нужную карту: ${(1-(factorial(userDeck-userProcent)/(factorial(userDraw)*(factorial((userDeck-userProcent)-userDraw))))/(factorial(userDeck)/(factorial(userDraw)*(factorial((userDeck)-userDraw)))))*100}%</p>`
+    document.getElementById('output').innerHTML+=`<p>Вероятность надровать гарнет: ${(1-(factorial(userDeck-userGarn)/(factorial(userDraw)*(factorial((userDeck-userGarn)-userDraw))))/(factorial(userDeck)/(factorial(userDraw)*(factorial((userDeck)-userDraw)))))*100}%</p>`
     if (userGarn>1){
-        document.getElementById('output').innerHTML+=`<p>Вероятность надровать 2 гарнета: ${(1-(factorial((userDeck-1)-(userGarn-1))/(factorial(4)*(factorial(((userDeck-1)-(userGarn-1))-4))))/(factorial(userDeck-1)/(factorial(4)*(factorial((userDeck-1)-4)))))*100}</p>%`
+        document.getElementById('output').innerHTML+=`<p>Вероятность надровать 2 гарнета: ${(1-(factorial((userDeck-1)-(userGarn-1))/(factorial(userDraw-1)*(factorial(((userDeck-1)-(userGarn-1))-(userDraw-1)))))/(factorial(userDeck-1)/(factorial(userDraw-1)*(factorial((userDeck-1)-(userDraw-1))))))*100}</p>%`
     }
 })
 function factorial(n) {
